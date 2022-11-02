1795f851bc9daff195315401ef87dacb24b5c995
Thomas McDonnell <thomas.mcdonnell@swrve.com>
2022-11-02 12:35:28 +0000

    Release release-2_0_2

--- SwrveSDK/tests/SwrveSDK.test.ts
+++ SwrveSDK/tests/SwrveSDK.test.ts
@@ -99,7 +99,7 @@ describe("SwrveSDK", () => {
     });
 
     it("must be the correct value", () => {
-      expect(SwrveSDK.getSDKVersion()).toBe("Web 2.0.1");
+      expect(SwrveSDK.getSDKVersion()).toBe("Web 2.0.2");
     });
   });
 
--- SwrveSDK/webpack.prod.js
+++ SwrveSDK/webpack.prod.js
@@ -2,6 +2,22 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 const sdkConfig = common[0];
+
+// Strip development logs
+sdkConfig.module.rules[0].use = sdkConfig.module.rules[0].use.concat(
+  [
+    {
+      loader: "strip-loader",
+      options: {
+        strip: [
+          "SwrveLogger.debug",
+          "SwrveLogger.info",
+          "SwrveLogger.warn",
+        ],
+      },
+    }
+  ]
+)
 const serviceWorkerConfig = common[1];
 
 const prod = {
